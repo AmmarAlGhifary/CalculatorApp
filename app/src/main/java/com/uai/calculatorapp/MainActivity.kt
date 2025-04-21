39f24e790c17907687e30477e46f7459235b308d
Ammar Al Ghifary <ammaralghifary@gmail.com>
2025-04-21 23:38:38 +0700

    Initial commit

---
+++ app/src/main/java/com/uai/calculatorapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.uai.calculatorapp
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.item_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_calculator -> {
+                true
+            }
+
+            R.id.menu_person -> {
+                true
+            }
+
+            R.id.menu_uai -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
