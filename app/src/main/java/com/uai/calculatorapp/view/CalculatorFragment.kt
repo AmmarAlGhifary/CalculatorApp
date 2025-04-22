a209a84875f807b1a8531deda798a0fc3414137e
Ammar Al Ghifary <ammaralghifary@gmail.com>
2025-04-22 22:58:18 +0700

    update

--- app/src/main/java/com/uai/calculatorapp/view/CalculatorFragment.kt
+++ app/src/main/java/com/uai/calculatorapp/view/CalculatorFragment.kt
@@ -5,13 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.uai.calculatorapp.R
 
 class CalculatorFragment : Fragment() {
+
+    private lateinit var btnClear: Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_calculator, container, false)
+        val view = inflater.inflate(R.layout.fragment_calculator, container, false)
+
+        btnClear = view.findViewById(R.id.btn_allClearCalc)
+        setOnClick()
+
+        return view
+    }
+
+    private fun setOnClick() {
+        btnClear.setOnClickListener {
+            clearChar()
+        }
+    }
+
+    private fun clearChar() {
+        TODO("Not yet implemented")
     }
 }
