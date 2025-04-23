833bb799f69b3b6fc16770118c2355ad42249cdc
Ammar Al Ghifary <ammaralghifary@gmail.com>
2025-04-24 05:26:40 +0700

    feat: Tambahkan logika dasar ke CalculatorFragment
    - Tambahkan RecyclerView untuk menampilkan tombol angka.
    - Implementasikan metode dasar (clearAll, clearChar, percentage, division, multiply, etc).
    - Atur onClick listener untuk tombol.
    Buat data class CalcButton.

---
+++ app/src/main/java/com/uai/calculatorapp/view/CalcButton.kt
@@ -0,0 +1,3 @@
+package com.uai.calculatorapp.view
+
+data class CalcButton()
--- app/src/main/java/com/uai/calculatorapp/view/CalculatorFragment.kt
+++ app/src/main/java/com/uai/calculatorapp/view/CalculatorFragment.kt
@@ -6,12 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.uai.calculatorapp.R
 
 class CalculatorFragment : Fragment() {
 
     private lateinit var btnClear: Button
+    private lateinit var btnClearChar: Button
+    private lateinit var btnPercntage: Button
+    private lateinit var btnDivision : Button
+    private lateinit var btnMultiply: Button
+    private lateinit var btnSubtract: Button
+    private lateinit var btnPlus : Button
+    private lateinit var btnEquals : Button
 
+
+    private lateinit var listNumber : RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,16 +32,73 @@ class CalculatorFragment : Fragment() {
         btnClear = view.findViewById(R.id.btn_allClearCalc)
         setOnClick()
 
+        listNumber.layoutManager = GridLayoutManager(context, 4)
+
         return view
     }
 
     private fun setOnClick() {
         btnClear.setOnClickListener {
+            clearAll()
+        }
+
+        btnClearChar.setOnClickListener {
             clearChar()
+        }
+
+        btnPercntage.setOnClickListener {
+            percentage()
+        }
+        btnDivision.setOnClickListener {
+            division()
+        }
+
+        btnMultiply.setOnClickListener {
+            multiply()
+        }
+
+        btnSubtract.setOnClickListener {
+            subtract()
+        }
+
+        btnPlus.setOnClickListener {
+            plus()
+        }
+
+        btnEquals.setOnClickListener {
+            equals()
         }
     }
 
+    private fun equals() {
+        TODO("Not yet implemented")
+    }
+
+    private fun plus() {
+
+    }
+
+    private fun subtract() {
+        TODO("Not yet implemented")
+    }
+
+    private fun multiply() {
+        TODO("Not yet implemented")
+    }
+
+    private fun division() {
+        TODO("Not yet implemented")
+    }
+
+    private fun percentage() {
+        TODO("Not yet implemented")
+    }
+
     private fun clearChar() {
+        TODO("Not yet implemented")
+    }
+
+    private fun clearAll() {
         TODO("Not yet implemented")
     }
 }
